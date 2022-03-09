0cffca977e34807d1b102a2b195f148aadaea140
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-03-09 20:28:33 +0530

    03-commit

--- app/src/main/java/com/aurosaswatraj/countmycrunch/MainActivity.kt
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/MainActivity.kt
@@ -30,24 +30,28 @@ class MainActivity : AppCompatActivity() {
             when(gender){
                 "Male"->{
                         if (weight_input.text.toString().isNotEmpty() &&
-                        height_input.text.toString().isNotEmpty() &&
-                        age_input.text.toString().isNotEmpty())
+                            heightin_input.text.toString().isNotEmpty() &&
+                            age_input.text.toString().isNotEmpty() &&
+                            heightft_input.text.toString().isNotEmpty())
                     {
                         take_input_BMI_Male_calculate()
                     }
                         else{
                             it.isEnabled = false
+//                        Alternative can be shown is a error message
                         }
                 }
                 "Female"->{
                     if (weight_input.text.toString().isNotEmpty() &&
-                        height_input.text.toString().isNotEmpty() &&
-                        age_input.text.toString().isNotEmpty())
+                        heightin_input.text.toString().isNotEmpty() &&
+                        age_input.text.toString().isNotEmpty() &&
+                        heightft_input.text.toString().isNotEmpty())
                     {
                         take_input_BMI_Female_calculate()
                     }
                     else{
                         it.isEnabled = false
+//                        Alternative can be shown is a error message
                     }
                 }
             }
@@ -57,37 +61,43 @@ class MainActivity : AppCompatActivity() {
 
     private fun take_input_BMI_Male_calculate(){
         var wt=weight_input.text.toString().toBigDecimal()
-        var ht=height_input.text.toString().toBigDecimal()
+        var ht_ft=heightft_input.text.toString().toBigDecimal()
+        var ht_in=heightin_input.text.toString().toBigDecimal()
         var age=age_input.text.toString().toBigDecimal()
 
-        calculate_BMI_Male(wt,ht,age)
+        calculate_BMI_Male(wt,ht_ft,ht_in,age)
     }
     private fun take_input_BMI_Female_calculate(){
         var wt=weight_input.text.toString().toBigDecimal()
-        var ht=height_input.text.toString().toBigDecimal()
+        var ht_ft=heightft_input.text.toString().toBigDecimal()
+        var ht_in=heightin_input.text.toString().toBigDecimal()
         var age=age_input.text.toString().toBigDecimal()
 
-        calculate_BMI_Female(wt,ht,age)
+        calculate_BMI_Female(wt,ht_ft,ht_in,age)
     }
 
-    private fun calculate_BMI_Female(wt: BigDecimal?, ht: BigDecimal?, age: BigDecimal?) {
+    private fun calculate_BMI_Female(wt: BigDecimal?, ht_ft:BigDecimal?,ht_in:BigDecimal?, age: BigDecimal?) {
         var female_weight=wt
-        var female_height=ht
+        var female_height_ft=ht_ft
+        var female_height_in=ht_in
         var female_age=age
-        TODO("To convert Feet and Inches both in XML and .ktfile")
-        TODO("Correct the below line")
-        TODO("Refer:https://www.inchcalculator.com/height-converter/")
-        TODO("Refer:https://www.baeldung.com/java-separate-double-into-integer-decimal-parts")
-        female_height="".toBigDecimal()
-        var BMR=66.toBigDecimal() +(13.7.toBigDecimal() * female_weight!!) + (5.toBigDecimal() * female_height!!) - (6.8.toBigDecimal() * female_age!!)
+
+        var heightincm= female_height_ft?.times(12.0.toBigDecimal())?.plus(female_height_in!!)
+        heightincm=heightincm?.times(2.54.toBigDecimal())
+
+        var female_height="".toBigDecimal()
+        var BMR=66.toBigDecimal() +(13.7.toBigDecimal() * female_weight!!) + (5.toBigDecimal() * heightincm!!) - (6.8.toBigDecimal() * female_age!!)
     }
 
-    private fun calculate_BMI_Male(wt:BigDecimal?,ht:BigDecimal?,age:BigDecimal?){
+    private fun calculate_BMI_Male(wt: BigDecimal?, ht_ft:BigDecimal?,ht_in:BigDecimal?, age: BigDecimal?){
 //        For Men
         var male_weight=wt
-        var male_height=ht?.times(2.54.toBigDecimal())
+        var male_height_ft=ht_ft
+        var male_height_in=ht_in
         var male_age=age
-        var BMR=655.toBigDecimal() +(9.6.toBigDecimal() * male_weight!!) + (1.8.toBigDecimal() * male_height!!) - (4.7.toBigDecimal() * male_age!!)
+        var heightincm= male_height_ft?.times(12.0.toBigDecimal())?.plus(male_height_in!!)
+        heightincm=heightincm?.times(2.54.toBigDecimal())
+        val BMR=655.toBigDecimal() +(9.6.toBigDecimal() * male_weight!!) + (1.8.toBigDecimal() * heightincm!!) - (4.7.toBigDecimal() * male_age!!)
     }
 
 }
@@ -98,3 +108,7 @@ class MainActivity : AppCompatActivity() {
  * Maintenance = 15-16 calories per #
  * Weight Gain = 18-19 calories per #
  */
+
+/* https://www.fruityvice.com/
+https://github.com/surhud004/Foodish
+* */
