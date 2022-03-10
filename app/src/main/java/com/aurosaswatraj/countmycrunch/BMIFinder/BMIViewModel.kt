7adbaba8de159188b8c8b4f42d9ffbfa34a35511
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-03-10 11:09:59 +0530

    03-commit-0.1

---
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/BMIFinder/BMIViewModel.kt
@@ -0,0 +1,8 @@
+package com.aurosaswatraj.countmycrunch.BMIFinder
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class BMIViewModel(application: Application) : AndroidViewModel(application) {
+//    Refer this: https://stackoverflow.com/questions/55914752/when-to-use-mutablelivedata-and-livedata
+}
