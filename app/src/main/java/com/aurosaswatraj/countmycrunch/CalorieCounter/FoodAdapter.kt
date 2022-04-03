dbcbe4f11b44acf7e20f7cf062e859587e5c6a31
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-04-03 18:18:02 +0530

    Edited food Calories

--- app/src/main/java/com/aurosaswatraj/countmycrunch/CalorieCounter/FoodAdapter.kt
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/CalorieCounter/FoodAdapter.kt
@@ -12,67 +12,29 @@ import com.aurosaswatraj.countmycrunch.R
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.food_list_item.view.*
 
-private const val TAG="FoodAdapter"
 class FoodViewHolder(override val containerView: View) :
     RecyclerView.ViewHolder(containerView), LayoutContainer{
     private val image: ImageView =containerView.findViewById(R.id.foodimage)
     private val name: TextView =containerView.findViewById(R.id.foodname)
-    private val cardBtn: CardView =containerView.findViewById(R.id.cardBtn)
-    private val addBtn: Button =containerView.findViewById(R.id.add_item_btn)
-    private val subBtn: Button =containerView.findViewById(R.id.sub_item_btn)
     private val textcounter: TextView =containerView.findViewById(R.id.num_item_text)
     fun bind( currentItem: FoodItems){
         image.setImageResource(currentItem.getMimgae())
         name.text = currentItem.getMtext()
         textcounter.text=currentItem.getMnoOfItems().toString()
 
-
-
-//        var counter=0
-//
-//        cardBtn.setOnClickListener {
-//            Log.d(TAG,"Card Clicked and Calorie is ${currentItem.getMCalorie()}")
-//        }
-//
-//        addBtn.setOnClickListener {
-//            counter++
-////            totalCalorie += currentItem.getMCalorie() * counter
-//            textcounter.text=counter.toString()
-//        }
-//
-//        subBtn.setOnClickListener {
-//            counter--
-////            totalCalorie -= currentItem.getMCalorie() * counter
-//            textcounter.text=counter.toString()
-//        }
-
     }
     }
 
 class FoodAdapter(val food:ArrayList<FoodItems>,private val selectListener:SelectListener): RecyclerView.Adapter<FoodViewHolder>() {
-
-
-
-    var totalCalorie=0.0
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodViewHolder {
         val view= LayoutInflater.from(parent.context).inflate(R.layout.food_list_item,parent,false)
         return FoodViewHolder(view)
     }
 
-
-
-    fun getCalorie():Double{
-        return totalCalorie
-    }
-
     override fun getItemCount(): Int {
         return food.size
     }
-
-
-
 
     override fun onBindViewHolder(holder: FoodViewHolder, position: Int) {
         val currentItem:FoodItems= food[position]
