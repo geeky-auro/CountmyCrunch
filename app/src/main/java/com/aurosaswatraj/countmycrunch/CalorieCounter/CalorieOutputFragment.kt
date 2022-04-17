ee5660f219a04f7ea727c13b35bfec2af7e292a8
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-04-17 12:54:44 +0530

    Start it from step-2

--- app/src/main/java/com/aurosaswatraj/countmycrunch/CalorieCounter/CalorieOutputFragment.kt
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/CalorieCounter/CalorieOutputFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.aurosaswatraj.countmycrunch.R
 import kotlinx.android.synthetic.main.fragment_calorie_output.*
 
@@ -20,13 +22,28 @@ private const val TAG="CalorieOutputFragment"
 class CalorieOutputFragment : Fragment(R.layout.fragment_calorie_output) {
 
 
+    private var viewpager: ViewPager2? =null
+
     private var listData=ArrayList<CalorieData>()
 //    private var adapter:ViewPagerAdapter?=null
 
     fun updateEditText(data: ArrayList<CalorieData>) {
         Log.d(TAG,"In OutputFragment $data")
         listData=data
+        val adapter=ViewPagerAdapter(listData)
+        viewpager?.adapter=adapter
 //        adapter=ViewPagerAdapter(data)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val v=inflater.inflate(R.layout.fragment_calorie_output,container,false)
+        viewpager=v.findViewById(R.id.viewPager)
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
 
@@ -34,8 +51,8 @@ class CalorieOutputFragment : Fragment(R.layout.fragment_calorie_output) {
         super.onViewCreated(view, savedInstanceState)
         Log.d(TAG,"onCreateView Started")
         Log.d(TAG,"onCreateView List Data:$listData")
-        val adapter=ViewPagerAdapter(listData)
-        viewPager.adapter=adapter
+//        val adapter=ViewPagerAdapter(listData)
+//        viewPager.adapter=adapter
 
     }
 
