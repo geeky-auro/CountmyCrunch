a7e9a9a69d0cccdeaabcff9ab2e2fad65da519c8
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-04-19 11:24:00 +0530

    Start it from step-2

---
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/manuals/UserManual.kt
@@ -0,0 +1,12 @@
+package com.aurosaswatraj.countmycrunch.manuals
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aurosaswatraj.countmycrunch.R
+
+class UserManual : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_manual)
+    }
+}
