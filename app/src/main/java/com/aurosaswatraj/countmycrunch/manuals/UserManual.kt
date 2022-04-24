4e07c74e84ac0847a5d9df6de8b3456e8f42caec
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-04-24 17:29:35 +0530

    Start it from step-2

--- app/src/main/java/com/aurosaswatraj/countmycrunch/manuals/UserManual.kt
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/manuals/UserManual.kt
@@ -2,10 +2,13 @@ package com.aurosaswatraj.countmycrunch.manuals
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.aurosaswatraj.countmycrunch.MainActivity
 import com.aurosaswatraj.countmycrunch.R
+import com.thecode.aestheticdialogs.*
 import kotlinx.android.synthetic.main.activity_user_manual.*
 
 
@@ -15,19 +18,27 @@ class UserManual : AppCompatActivity() {
         setContentView(R.layout.activity_user_manual)
 
         web.loadUrl("https://medium.com/@aurosaswatraj/user-manual-of-calorie-counter-972df34a8009")
-        web.getSettings().setJavaScriptEnabled(true)
+        web.settings.javaScriptEnabled = true
 
         // WebViewClient allows you to handle
         // onPageFinished and override Url loading.
 
         // WebViewClient allows you to handle
         // onPageFinished and override Url loading.
-        web.setWebViewClient(WebViewClient())
 
+
+
+        web.setWebViewClient(object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+
+            }
+        })
         DONEi.setOnClickListener {
             val i = Intent(applicationContext, MainActivity::class.java)
             startActivity(i)
             finish()
         }
+
+
     }
 }
