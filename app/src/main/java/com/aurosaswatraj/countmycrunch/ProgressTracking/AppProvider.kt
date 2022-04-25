b1e76cef5cf1d7fb420492dbf960b5c45a15db81
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-04-25 19:21:13 +0530

    Start it from step-2

--- app/src/main/java/com/aurosaswatraj/countmycrunch/ProgressTracking/AppProvider.kt
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/ProgressTracking/AppProvider.kt
@@ -2,8 +2,10 @@ package com.aurosaswatraj.countmycrunch.ProgressTracking
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
+import android.util.Log
 
 
 private const val TAG = "AppProvider"
@@ -17,6 +19,37 @@ val CONTENT_AUTHORITY_URI: Uri = Uri.parse("content://$CONTENT_AUTHORITY")
 
 
 class AppProvider:ContentProvider(){
+
+
+    private val uriMatcher by lazy { buildUriMatcher() }
+
+    private fun buildUriMatcher(): UriMatcher {
+        Log.d(TAG, "buildURIMatcher starts")
+
+//      to match the content URI
+//      every time user access table under content provider
+
+        val matcher = UriMatcher(UriMatcher.NO_MATCH)
+
+//        to access whole table
+//        e.g. content://com.aurosaswatraj.tasktimer.provider/Tasks
+        matcher.addURI(CONTENT_AUTHORITY, TrackContract.TABLE_NAME, TRACKS)
+
+//        to access a particular row
+//        of the table
+//        e.g. content://com.aurosaswatraj.tasktimer.provider/Tasks/8
+        matcher.addURI(CONTENT_AUTHORITY, "${TrackContract.TABLE_NAME}/#", TRACKS_ID)
+//
+
+//
+//
+//        matcher.addURI(CONTENT_AUTHORITY,DurationsContract.TABLE_NAME, TASKS_DURATIONS)
+//        matcher.addURI(CONTENT_AUTHORITY,"${DurationsContract.TABLE_NAME}/#", TASKS_DURATIONS_ID)
+//
+
+        return matcher
+    }
+
     override fun onCreate(): Boolean {
         TODO("Not yet implemented")
     }
