318c52dd7001618d53a7deaf1928527478259ff9
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-04-27 15:50:44 +0530

    Start it from step-2

---
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/ProgressTracking/TrackViewModel.kt
@@ -0,0 +1,20 @@
+package com.aurosaswatraj.countmycrunch.ProgressTracking
+
+
+/**
+ * Instead of extending the ViewModel class we'll extend an Android view model.
+Both classes work the same way.
+AndroidViewModel is a subclass of ViewModel,
+but it also has an application property.
+That's going to be useful if we want to access the database,
+because we'll need an application to get a ContentResolver.
+ *
+ *
+ * */
+
+
+private const val TAG = "TrackViewModel"
+
+class TrackViewModel {
+
+}
