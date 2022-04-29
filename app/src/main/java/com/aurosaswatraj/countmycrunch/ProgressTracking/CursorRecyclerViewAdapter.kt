914ca8c0d45633e15ebf03116d512fd8ee83be3f
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-04-29 14:50:56 +0530

    Objective 4 Needs to be completed

---
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/ProgressTracking/CursorRecyclerViewAdapter.kt
@@ -0,0 +1,18 @@
+package com.aurosaswatraj.countmycrunch.ProgressTracking
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.extensions.LayoutContainer
+
+
+class TaskViewHolder(override val containerView: View) :
+    RecyclerView.ViewHolder(containerView), LayoutContainer{
+//    By using an interface We can notify the fragment/activity that a button is tapped
+//    MainActivityFragment will implement the functions in the interface,
+//    and we can be sure that it contains the functions that we're going to call.
+    }
+
+
+
+class CursorRecyclerViewAdapter {
+}
