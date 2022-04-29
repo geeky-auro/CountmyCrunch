570e4647b084a252e5b935be3e48bd84056c5b6f
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-04-29 14:52:16 +0530

    Objective 4 Needs to be completed

--- app/src/main/java/com/aurosaswatraj/countmycrunch/ProgressTracking/CursorRecyclerViewAdapter.kt
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/ProgressTracking/CursorRecyclerViewAdapter.kt
@@ -10,9 +10,21 @@ class TaskViewHolder(override val containerView: View) :
 //    By using an interface We can notify the fragment/activity that a button is tapped
 //    MainActivityFragment will implement the functions in the interface,
 //    and we can be sure that it contains the functions that we're going to call.
+
+    fun bind(task:Track,listener :CursorRecyclerViewAdapter.onTaskClickListener){
+
+    }
     }
 
 
 
 class CursorRecyclerViewAdapter {
+
+    interface onTaskClickListener{
+        fun onEditClick(task:Track)
+        fun onDeleteClick(task:Track)
+        fun onTaskLongClick(task:Track)
+//      Now that we've defined the interface, we can pass in a reference to something that implements that interface,
+//      so that the adapter knows what to call.(Adding in the primary Constructor)
+    }
 }
