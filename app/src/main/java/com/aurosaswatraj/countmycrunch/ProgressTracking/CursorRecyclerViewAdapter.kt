b74ad6c1f5be04c82c7f3f30faf460a420cd420a
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-04-29 15:06:14 +0530

    Objective 4 Needs to be completed

--- app/src/main/java/com/aurosaswatraj/countmycrunch/ProgressTracking/CursorRecyclerViewAdapter.kt
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/ProgressTracking/CursorRecyclerViewAdapter.kt
@@ -1,9 +1,15 @@
 package com.aurosaswatraj.countmycrunch.ProgressTracking
 
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.aurosaswatraj.countmycrunch.R
 import kotlinx.android.extensions.LayoutContainer
 
+
+private const val TAG = "CursorRecyclerViewAdapt"
 
 class TaskViewHolder(override val containerView: View) :
     RecyclerView.ViewHolder(containerView), LayoutContainer{
@@ -18,13 +24,28 @@ class TaskViewHolder(override val containerView: View) :
 
 
 
-class CursorRecyclerViewAdapter {
+class CursorRecyclerViewAdapter:RecyclerView.Adapter<TaskViewHolder>() {
 
     interface onTaskClickListener{
         fun onEditClick(task:Track)
         fun onDeleteClick(task:Track)
 //      Now that we've defined the interface, we can pass in a reference to something that implements that interface,
 //      so that the adapter knows what to call.(Adding in the primary Constructor)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        Log.d(TAG, "onCreateViewHolder STARTS: new View Requested.!")
+        val view =LayoutInflater.from(parent.context).inflate(R.layout.task_list_item, parent, false)
+        return TaskViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
     }
 
 }
