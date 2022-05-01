0b9a24a8e899c0a68e09f7f17a23a4c385a339d0
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-05-02 00:20:00 +0530

    Objective 4 Needs to be completed

--- app/src/main/java/com/aurosaswatraj/countmycrunch/ProgressTracking/MainActivityFragment.kt
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/ProgressTracking/MainActivityFragment.kt
@@ -13,10 +13,8 @@ import com.aurosaswatraj.countmycrunch.R
 import kotlinx.android.synthetic.main.fragment_main_activity.*
 import java.lang.RuntimeException
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
+
 private const val TAG = "MainActivityFragment"
 
 //    Create a ViewModel instance..
@@ -27,9 +25,7 @@ private const val TAG = "MainActivityFragment"
  * create an instance of this fragment.
  */
 class MainActivityFragment : Fragment(),CursorRecyclerViewAdapter.onTaskClickListener {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     private val viewModel by lazy {
         // ViewModelProviders.of(activity!!).get(TaskTimerViewModel::class.java)
@@ -77,25 +73,6 @@ class MainActivityFragment : Fragment(),CursorRecyclerViewAdapter.onTaskClickLis
         return inflater.inflate(R.layout.fragment_main_activity, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainActivityFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainActivityFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 
     override fun onEditClick(task: Track) {
         TODO("Not yet implemented")
@@ -103,5 +80,7 @@ class MainActivityFragment : Fragment(),CursorRecyclerViewAdapter.onTaskClickLis
 
     override fun onDeleteClick(task: Track) {
         viewModel.deleteTask(task.id)
+        /**The onDeleteClick function in MainActivityFragment will then call the ViewModel's delete task function,
+        to tell the ViewModel to delete the task.**/
     }
 }
