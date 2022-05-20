b47beffb8e1d76f372b81f98bb0ed0ed03d02780
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-05-20 19:33:56 +0530

    Implement Graph

---
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/HealthBlogs/HealthVlogs.kt
@@ -0,0 +1,12 @@
+package com.aurosaswatraj.countmycrunch.HealthBlogs
+
+class HealthVlogs(val image:String,val title:String,val Description:String,val vlogLink:String) {
+
+
+   /**
+    * vlogs.add(HealthVlogs("https://artofhealthyliving.com/wp-content/uploads/2022/05/custom-orthotics-to-help-with-foot-pain.jpg","The Art of Healthy Living","ur feet perform a heavy workload daily. And being squeezed into tight shoes doesn’t help either. Shouldering your weight around day and night, it’s a relief to them when you head to bed at night, and they finally get to rest. Your feet literally say “aah!”","https://artofhealthyliving.com/wearing-these-pillowy-custom-orthotics-your-feet-will-say-aah/"))
+    * vlogs.add(HealthVlogs("https://artofhealthyliving.com/wp-content/uploads/2022/05/Healthy-Living-The-Benefits-Of-Water-Exercise.jpg","HEALTHY LIVING: THE BENEFITS OF WATER EXERCISE","Short Description of the article","https://artofhealthyliving.com/wearing-these-pillowy-custom-orthotics-your-feet-will-say-aah/"))
+    *
+    * */
+
+}
