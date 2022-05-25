dae0b8c70c50c3ceb4928fe9bff34358cc765ae0
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-05-25 13:42:23 +0530

    Implement Graph

---
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/DashBoard/DashBoard.kt
@@ -0,0 +1,14 @@
+package com.aurosaswatraj.countmycrunch.DashBoard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aurosaswatraj.countmycrunch.R
+
+class DashBoard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dashboard_ui)
+    }
+
+// TODO: Implement a spotlight feature-> https://github.com/TakuSemba/Spotlight
+}
