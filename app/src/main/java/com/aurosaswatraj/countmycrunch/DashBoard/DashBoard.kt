61d1af810ca9a1d65d681e6baebf0acfedcce49b
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-06-04 20:11:04 +0530

    UI Dashboard Updated..!

--- app/src/main/java/com/aurosaswatraj/countmycrunch/DashBoard/DashBoard.kt
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/DashBoard/DashBoard.kt
@@ -1,13 +1,20 @@
 package com.aurosaswatraj.countmycrunch.DashBoard
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.aurosaswatraj.countmycrunch.R
 
 class DashBoard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dashboard_ui)
+        val window = this.window
+        window.statusBarColor = Color.parseColor("#2f3640")
+
+
+
+
     }
 // TODO: Implement a Cardview based UI DashBoard : https://www.geeksforgeeks.org/dashboard-ui-design-in-android/
 // TODO: Implement a spotlight feature-> https://github.com/TakuSemba/Spotlight
