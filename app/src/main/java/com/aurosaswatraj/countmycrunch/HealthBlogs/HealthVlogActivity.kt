feded46a7fdb20cc0e4f9b6bd71d4a9bab090797
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-06-07 18:43:39 +0530

    Swiping Feature Updated..!

--- app/src/main/java/com/aurosaswatraj/countmycrunch/HealthBlogs/HealthVlogActivity.kt
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/HealthBlogs/HealthVlogActivity.kt
@@ -1,13 +1,14 @@
 package com.aurosaswatraj.countmycrunch.HealthBlogs
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.aurosaswatraj.countmycrunch.R
 import kotlinx.android.synthetic.main.activity_health_vlogs.*
 import java.util.*
+
 
 class HealthVlogActivity : AppCompatActivity(),iSelectListener {
 
@@ -24,10 +25,18 @@ class HealthVlogActivity : AppCompatActivity(),iSelectListener {
         vlogs.shuffle()
         recyclerVlogs.adapter=VlogAdapter(vlogs,this)
 
+        swipeRefreshLayout.setOnRefreshListener {
+            swipeRefreshLayout.isRefreshing = false
+            RearrangeItems()
+        }
 
 
+    }
 
-
+    fun RearrangeItems() {
+        // Shuffling the data of ArrayList using system time
+        vlogs.shuffle(Random(System.currentTimeMillis()))
+        recyclerVlogs.adapter=VlogAdapter(vlogs,this)
     }
 
     override fun onVlogSelected(vlogLink: String?) {
