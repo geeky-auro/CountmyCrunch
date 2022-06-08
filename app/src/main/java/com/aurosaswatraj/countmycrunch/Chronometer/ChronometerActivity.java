92c06a7aaa5f8a688342cf894592ed2b2c9f9375
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-06-08 18:40:32 +0530

    Objective 6 Animations with asset Code Added..!

--- app/src/main/java/com/aurosaswatraj/countmycrunch/Chronometer/ChronometerActivity.java
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/Chronometer/ChronometerActivity.java
@@ -17,7 +17,7 @@ public class ChronometerActivity extends AppCompatActivity {
 
 
 
-    Button btnstart;
+    Button btnstart,btnstop;
     ImageView icanchor;
     Animation roundingalone;
 
@@ -28,7 +28,13 @@ public class ChronometerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chronometer);
 
         btnstart=findViewById(R.id.btn_start);
+        btnstop=findViewById(R.id.btn_stop);
         icanchor=findViewById(R.id.icanchor);
+
+//        create optional aniamtion
+        btnstop.setAlpha(0);
+
+
 
 //        Load Animation
         roundingalone= AnimationUtils.loadAnimation(this,R.anim.roundingalone);
@@ -36,11 +42,16 @@ public class ChronometerActivity extends AppCompatActivity {
 //                Set fonts
         Typeface MMedium=Typeface.createFromAsset(getAssets(),"fonts/mmedium.ttf");
 
+        btnstart.setTypeface(MMedium);
+        btnstop.setTypeface(MMedium);
+
         btnstart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 //                passing Animation
                 icanchor.startAnimation(roundingalone);
+                btnstop.animate().alpha(1).translationY(-80).setDuration(300).start();
+                btnstart.animate().alpha(0).setDuration(300).start();
             }
         });
 
