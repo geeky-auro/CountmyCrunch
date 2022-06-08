01c28f88dc578b741f2f6aca7699a9579632f6be
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-06-08 17:36:12 +0530

    Objective Files Added..!

---
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/Chronometer/ChronometerActivity.java
@@ -0,0 +1,16 @@
+package com.aurosaswatraj.countmycrunch.Chronometer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aurosaswatraj.countmycrunch.R;
+
+public class ChronometerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chronometer);
+    }
+}
