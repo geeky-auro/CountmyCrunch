369533e70f7fe025f2bbf641e3f91e65dca5931b
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-06-16 19:06:34 +0530

    Spinkit updated

--- app/src/main/java/com/aurosaswatraj/countmycrunch/HealthBlogs/VlogAdapter.kt
+++ app/src/main/java/com/aurosaswatraj/countmycrunch/HealthBlogs/VlogAdapter.kt
@@ -8,12 +8,19 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.aurosaswatraj.countmycrunch.R
+import com.google.gson.JsonObject
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import io.supercharge.shimmerlayout.ShimmerLayout
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_vlogs.view.*
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
 import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.URL
 
 class VlogsViewHolder(override val containerView: View) :
     RecyclerView.ViewHolder(containerView), LayoutContainer {
@@ -23,12 +30,12 @@ class VlogsViewHolder(override val containerView: View) :
     private val shimmer_img:ShimmerLayout=containerView.findViewById(R.id.shimmer_image)
 
 
-
     var picasso =Picasso.Builder(containerView.getContext()).listener { picasso, uri, exception ->
             Log.d("TAG", " onImageLoadFailed message")
 
             exception.printStackTrace()
         }.build()
+
 
 
     fun bind( currentItem: HealthVlogs){
@@ -46,7 +53,7 @@ class VlogsViewHolder(override val containerView: View) :
 
                 override fun onError(e: Exception?) {
 
-                    val generateRandomeImageNum:Int=(Math.random()*(6-1+1)+1).toInt()
+                    val generateRandomeImageNum:Int=(Math.random()*(16-1+1)+1).toInt()
                     val imageLink:String
                     shimmer_img.stopShimmerAnimation()
                     when(generateRandomeImageNum){
@@ -68,6 +75,37 @@ class VlogsViewHolder(override val containerView: View) :
                         6->{
                             imageLink="https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                         }
+                        7->{
+                            imageLink="https://images.pexels.com/photos/4753928/pexels-photo-4753928.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        }
+                        8->{
+                            imageLink="https://images.pexels.com/photos/4473622/pexels-photo-4473622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        }
+                        9->{
+                            imageLink="https://images.pexels.com/photos/4384679/pexels-photo-4384679.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        }
+                        10->{
+                            imageLink="https://images.pexels.com/photos/9637936/pexels-photo-9637936.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        }
+                        11->{
+                            imageLink="https://images.pexels.com/photos/1117493/pexels-photo-1117493.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        }
+                        12->{
+                            imageLink="https://images.pexels.com/photos/931324/pexels-photo-931324.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        }
+                        13->{
+                            imageLink="https://images.pexels.com/photos/2922455/pexels-photo-2922455.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        }
+                        14->{
+                            imageLink="https://images.pexels.com/photos/842571/pexels-photo-842571.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        }
+                        15->{
+                            imageLink="https://images.pexels.com/photos/3622476/pexels-photo-3622476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        }
+                        16->{
+                            imageLink="https://images.pexels.com/photos/70078/pexels-photo-70078.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        }
+
                         else->{
                             imageLink="https://images.pexels.com/photos/5907810/pexels-photo-5907810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                         }
